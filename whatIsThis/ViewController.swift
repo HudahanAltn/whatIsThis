4e09f637fd86b7f1c9ab27c1f1cd40b5408bf80c
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-02-12 15:10:36 +0300

    created two View Controller. VC1 is main page.VC2 is information page.Visual objects connected to VC1 and VC2. auto layout configurated

--- whatIsThis/ViewController.swift
+++ whatIsThis/ViewController.swift
@@ -9,11 +9,23 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    
+    @IBOutlet weak var informationLabel: UILabel!
+    
+    @IBOutlet weak var questionMarkImageView: UIImageView!
+    
+    @IBOutlet weak var takeAPictureButton: UIButton!
+    
+    @IBOutlet weak var chooseAPictureButton: UIButton!
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view.
     }
 
-
+    @IBAction func takeAPicturePressed(_ sender: UIButton) {
+    }
+    
+    @IBAction func chooseAPicturePressed(_ sender: Any) {
+    }
 }
 
---
+++ whatIsThis/ViewController2.swift
@@ -0,0 +1,24 @@
+//
+//  ViewController2.swift
+//  whatIsThis
+//
+//  Created by Hüdahan Altun on 12.02.2023.
+//
+
+import UIKit
+
+class ViewController2: UIViewController {
+
+    
+    @IBOutlet weak var objectImageView: UIImageView!
+    
+    @IBOutlet weak var objectNameLabel: UILabel!
+    @IBOutlet weak var objectInformationTextView: UITextView!
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+}
