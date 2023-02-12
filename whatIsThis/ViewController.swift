c5a3dea7a2d28268e285666765b65f3045f5eb48
HudahanAltn <dev.hudahanaltun@gmail.com>
2023-02-12 15:48:19 +0300

    image picking is done.added some animations

--- whatIsThis/ViewController.swift
+++ whatIsThis/ViewController.swift
@@ -17,15 +17,74 @@ class ViewController: UIViewController {
     @IBOutlet weak var takeAPictureButton: UIButton!
     
     @IBOutlet weak var chooseAPictureButton: UIButton!
+    
+    @IBOutlet weak var progressView: UIProgressView!
+    
+    var imagePicker = UIImagePickerController ()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        imagePicker.delegate = self
+        progressView.progress = 0
+        
+    }
+    override func viewWillAppear(_ animated: Bool) {
+        progressView.progress = 0
     }
 
     @IBAction func takeAPicturePressed(_ sender: UIButton) {
+        imagePicker.sourceType = .camera
+        present(imagePicker, animated: true)
     }
     
     @IBAction func chooseAPicturePressed(_ sender: Any) {
+        imagePicker.sourceType = .photoLibrary
+        present(imagePicker, animated: true)
     }
+    
 }
 
+extension ViewController{
+    
+    func detectObject(image:CIImage){
+        
+    }
+    
+    @objc func gotoVC2(){
+        
+        self.performSegue(withIdentifier: "toVC2", sender: nil)
+    }
+    
+}
+
+extension ViewController:UIImagePickerControllerDelegate,UINavigationControllerDelegate{
+    
+    func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [UIImagePickerController.InfoKey : Any]) {
+        
+        if let userPickedImage = info[UIImagePickerController.InfoKey.editedImage] as? UIImage{
+            //image that user picked is stored like dictionary. and we must take original photo with "original ımage" method.and we converted the incoming image to UIImage  with the "if let"
+            
+            
+            guard let ciImage = CIImage(image: userPickedImage) else{//we have to convert ciImage to use with .mlmodel
+                fatalError("could not convert UIImage into CIImage")
+            }
+            
+        
+            detectObject(image: ciImage)//passed image to our func
+        }
+        
+       
+        imagePicker.dismiss(animated: true,completion: nil)// camera will close and main page will open
+        
+        UIView.animate(withDuration: 5, delay: 4, animations: {
+            
+            self.progressView.setProgress(1, animated: true)
+            
+        })
+        
+        _ = Timer.scheduledTimer(timeInterval: 6, target: self, selector: #selector(gotoVC2), userInfo: nil, repeats: false)
+        
+    }
+    
+}
